23ed52bad51f356b934192f63a0de0453f4b342f
Bart Massey <bart.massey@gmail.com>
2021-03-12 23:07:45 -0800

    got some string encode / decode stuff done

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,12 @@ mod strings;
 pub use scalars::*;
 pub use strings::*;
 
-pub use encoding;
+pub use encoding_rs;
+pub use codepage;
+pub use oem_cp;
 
+#[non_exhaustive]
 pub enum ConvertError {
     Range,
+    Encoding,
 }
--- src/strings.rs
+++ src/strings.rs
@@ -1,8 +1,43 @@
 use crate::*;
 
-use encoding::types::Encoding;
+use std::borrow::Cow;
+
+#[non_exhaustive]
+pub enum Coding {
+    ERS(encoding_rs::Encoding),
+    OCC {
+        encode: &'static oem_cp::ahash::AHashMap<char, u8>,
+        decode: oem_cp::code_table_type::TableType,
+    }
+}
 
 type ByteString = Box<[u8]>;
+
+impl Coding {
+    fn encode<'a, S>(&'static self, src: S) -> Result<ByteString, ConvertError>
+        where S: Into<Cow<'a, str>>
+    {
+        match self {
+            Coding::ERS(c) => {
+                let src = src.into();
+                let oe = c.output_encoding();
+                let (out, _, ok) = oe.encode(src.as_ref());
+                if ok {
+                    Ok(out.to_owned().to_vec().into_boxed_slice())
+                } else {
+                    Err(ConvertError::Encoding)
+                }
+            }
+            Coding::OCC { encode: et, .. } => {
+                match oem_cp::encode_string_checked(src, et) {
+                    Some(out) => Ok(out.into_boxed_slice()),
+                    None => Err(ConvertError::Encoding),
+                }
+            },
+        }
+    }
+}
+
 
 macro_rules! native_type {
     ($rty:ident) => {
@@ -19,7 +54,6 @@ native_type!(BZSTR);
 native_type!(WZSTR);
 
 pub trait NativeString: Sized {
-    fn to_native(&self, enc: &dyn Encoding) -> Result<String, ConvertError>;
-    fn from_native(v: &str, enc: &dyn Encoding) -> Result<Self, ConvertError>;
+    fn to_native(&self, enc: &Coding) -> Result<String, ConvertError>;
+    fn from_native(v: &str, enc: &Coding) -> Result<Self, ConvertError>;
 }
-
